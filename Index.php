082615ec66ef3e714cace5b7d18544745d36a48b
ByronSolorzano <basfuentes12@hotmail.com>
2017-02-13 23:24:00 -0600

    Se agrego el Nav

---
+++ Index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang= "ES">
+    <head>
+        <title>MirrorWatch</title>
+        <!--Se agrega el tipo de idioma-->
+        <meta chartset="UTF-8"/>
+        <!-- Dispositivo mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--Se llama el CSS de Bootstrap-->
+        <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"  media="screen,projection"/>
+        <!--Se llama el CSS propio-->
+         <link type="text/css" rel="stylesheet" href="css/Estilos.css"  media="screen,projection"/>
+        <!--Importar fuentes-->
+        <link href="fonts/glyphicons-halflings-regular.woff2" rel="stylesheet">
+    </head>
+    <body>
+
+        <!--Se agrega en menu navegador-->
+        <header>
+        <div class="container-fluid ">
+            <div class="row">
+                <div class="col-xs-3 col-sm-4 col-md-4 col-lg-3">
+                    <a href="#"><img src="img/Logo.png" class="im-po"/></a>
+                 <nav>
+                    <ul>
+                        <li><a href="#"></a>Productos</li>
+                        <li><a href="#"></a>Servicios</li>
+                        <li><a href="#"></a>Nosotros</li>
+                    </ul>
+                    </nav>
+                </div>
+
+                
+                  <div class="col-xs-9 col-sm-8 col-md-8 col-lg-9 parallax">
+                   
+                  </div>                  
+            </div>
+        </div>
+        </header>
+
+        <section class="main container">
+            <div class="row">
+
+            <p>
+                as,fjahsflshdfskdjf
+            </p>
+
+            </div>
+        </section>
+
+        
+       
+
+
+    <!--Importar jQuery y Js de boots-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="js/inicia.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    </body>
+</html>
